5c5df415f3fe3453d710d3f521c42801b3305b78
Simone Orru <simone.orru@secomind.com>
2026-05-04 09:13:01 +0200

    Highlight issue with ethernet and mcuboot for NXP boards with ksz8081
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- samples/sysbuild/with_mcuboot/src/main.c
+++ samples/sysbuild/with_mcuboot/src/main.c
@@ -11,5 +11,10 @@ int main(void)
 {
 	printk("Address of sample %p\n", (void *)__rom_region_start);
 	printk("Hello sysbuild with mcuboot! %s\n", CONFIG_BOARD);
+	printk("Waiting for PHY auto-negotiation to trigger in the background...\n");
+
+	/* Sleep to keep the main thread alive while the network stack logs the bug */
+	k_sleep(K_SECONDS(10));
+
 	return 0;
 }
